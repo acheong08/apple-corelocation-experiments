7f6d7b507b0a66d152ce3380697c6ecddecc84f5
Antonio Cheong <acheong@duti.dev>
2024-06-05 17:43:59 +0800

    add pack/unpack to morton cmd

--- cmd/morton/main.go
+++ cmd/morton/main.go
@@ -5,7 +5,6 @@ import (
 	"log"
 	"wloc/lib/morton"
 
-	"github.com/buckhx/tiles"
 	"github.com/leaanthony/clir"
 )
 
@@ -30,17 +29,26 @@ func main() {
 		fmt.Println(lat, long)
 		return nil
 	})
-	experiment := cli.NewSubCommand("experiment", "Experimental encoding")
-	experiment.Float64Flag("lat", "latitude", &lat)
-	experiment.Float64Flag("long", "longitude", &long)
-	experiment.Action(func() error {
-		t := tiles.FromCoordinate(lat, long, 13)
-		p := t.ToPixel()
-		t2, _ := p.ToTile()
-		tileKey := morton.Pack(t2.Y, t2.X)
-		fmt.Println(tileKey)
+
+	var mLat int
+	var mLong int
+	pack := cli.NewSubCommand("pack", "Pack quadkey encoded coordinates to tilekey")
+	pack.IntFlag("lat", "latitude", &mLat)
+	pack.IntFlag("long", "longitude", &mLong)
+	pack.Action(func() error {
+		tileKey := morton.Pack(mLat, mLong)
+		fmt.Println("Tile Key: ", tileKey)
 		return nil
 	})
+
+	unpack := cli.NewSubCommand("unpack", "Unpack tilekey to quadkey encoded coordinates")
+	unpack.Int64Flag("tile", "tile key", &tileKey)
+	unpack.Action(func() error {
+		mLat, mLong := morton.Unpack(tileKey)
+		fmt.Println(mLat, mLong)
+		return nil
+	})
+
 	err := cli.Run()
 	if err != nil {
 		panic(err)
