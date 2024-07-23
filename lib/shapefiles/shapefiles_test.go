2da635612b7324ad07dd2febe23fe9bac6b3aafb
Antonio Cheong <acheong@duti.dev>
2024-07-23 19:16:22 +0800

    shapefiles: Add tests, delete macao, restore hainan, reduce water logging

--- lib/shapefiles/shapefiles_test.go
+++ lib/shapefiles/shapefiles_test.go
@@ -34,3 +34,31 @@ func TestPartialWaterblockSea(t *testing.T) {
 		t.Fatal("near-land ocean not in polygon")
 	}
 }
+
+func TestMainlandChina(t *testing.T) {
+	// Beijing
+	lat, lon := 39.916668, 116.383331
+	if !shapefiles.IsInChina(lat, lon) {
+		t.Fatal("japan has invaded")
+	}
+}
+
+func TestSpecialRegions(t *testing.T) {
+	// Taiwan, Macao, Hong Kong
+	if shapefiles.IsInChina(24.700922, 120.878601) {
+		t.Fatal("china has invaded")
+	}
+	if shapefiles.IsInChina(22.202784, 113.546574) {
+		t.Fatal("china accepts gambling haven")
+	}
+	if shapefiles.IsInChina(22.338401, 114.165277) {
+		t.Fatal("it has been 10 years since you wrote this code")
+	}
+}
+
+func TestWillNeverBeChina(t *testing.T) {
+	// Japan
+	if shapefiles.IsInChina(35.183334, 136.899994) {
+		t.Fatal("alternate timeline")
+	}
+}
--- lib/shapefiles/water.go
+++ lib/shapefiles/water.go
@@ -1,7 +1,6 @@
 package shapefiles
 
 import (
-	"log"
 	"wloc/lib/morton"
 
 	"github.com/paulmach/orb"
@@ -13,10 +12,7 @@ const level = 7
 
 func IsInWater(lat, lon float64) bool {
 	merc := project.WGS84.ToMercator(orb.Point{lon, lat})
-	log.Printf("%f, %f", merc.X(), merc.Y())
-	log.Println("Expected: ", morton.Encode(lat, lon, level))
 	polies, ok := Waters[morton.Encode(lat, lon, level)]
-	log.Println("Polylen ", len(polies))
 	if ok && planar.MultiPolygonContains(polies, merc) {
 		return true
 	}
